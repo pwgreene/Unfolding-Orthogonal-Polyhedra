706d46c948bcfee22323db5766d38b1f5b1b97e1
msangha <manjot@mit.edu>
2017-04-30 23:59:42 -0400

    fixed broken graph class

--- src/graph.py
+++ src/graph.py
@@ -7,9 +7,9 @@ class Graph:
   # E is edges as a list of lists where list i is the list of indices of vertices vertex i is connected to
   # E_long_list is a list of all edges where an edge is a list of length 2 of the indices of the vertices it connects, edges are assumed bidirectional and should not be input twice (eg. if [0, 1] is given [1, 0] should not be)
   def __init__(self, V, E=None, E_list=None):
-    if not E and not E_long_list:
+    if not E and not E_list:
       raise Exception("No edges inputted")
-    elif E and E_long_list:
+    elif E and E_list:
       raise Exception("Can't input edges in multiple formats")
     self.V = V
     if E:
@@ -18,7 +18,7 @@ class Graph:
       self.E = []
       for _ in xrange(len(V)):
         self.E.append([])
-      for edge in E_long_list:
+      for edge in E_list:
         # assumes bi-directional
         self.E[edge[0]].append(edge[1])
         self.E[edge[1]].append(edge[0])
