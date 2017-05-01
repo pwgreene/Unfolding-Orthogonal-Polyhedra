c6eb369e8457da1b1bce4637b15e47d46bc661bb
Parker Greene <pwgreene@mit.edu>
2017-04-30 21:25:01 -0400

    added a parser for fold files into faces

--- src/faces.py
+++ src/faces.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class Face(object):
 
-    def __init__(self, vertices, face_type, direction):
+    def __init__(self, vertices, direction):
         """
         initialize the face, vertices v1, v2, v3, v4 (np arrays) should be defined in ccw order
         edges of the face are (v1, v2), (v2, v3), (v3, v4), and (v4, v1)
@@ -12,8 +12,7 @@ class Face(object):
         direction: +- x, +- y, or +- z (string)
         """
         self.vertices = vertices
-        self.type = face_type
-        self.pricipal_directions = ["x", "y", "z"]
+        self.principal_directions = ["x", "y", "z"]
         if len(direction) == 1:
             self.direction = "+"+direction
         else:
@@ -41,7 +40,7 @@ class Face(object):
             return direction
 
     def __str__(self):
-        return "%s, %s, %s, %s" % self.vertices
+        return ("(%s, %s, %s, %s) direction: "+self.direction) % tuple(self.vertices)
 
 
     def divide_face(self, axis, ratio):
@@ -51,28 +50,28 @@ class Face(object):
         ratio: len 2 tup of float or string
         :return: two new Faces
         """
-        assert axis in self.pricipal_directions and len(ratio) == 2
+        assert axis in self.principal_directions and len(ratio) == 2
         total = sum(ratio)
         w1 = ratio[0]/float(total)
         w2 = ratio[1]/float(total)
         # check the axis and find midpoint accordingly
-        for i in range(len(self.pricipal_directions)):
-            if axis == self.pricipal_directions[i]:
+        for i in range(len(self.principal_directions)):
+            if axis == self.principal_directions[i]:
                 # first/third edges parallel to axis
                 if (self.vertices[0]-self.vertices[1])[i] != 0:
                     mid = w1*self.vertices[0][i] + w2*self.vertices[1][i]
                     new_v1, new_v2 = self.vertices[1].copy(), self.vertices[2].copy()
                     new_v1[i], new_v2[i] = mid, mid
-                    return (Face([self.vertices[0], new_v1, new_v2, self.vertices[3]], self.type, self.direction),
-                            Face([new_v1, self.vertices[1], self.vertices[2], new_v2], self.type, self.direction))
+                    return (Face([self.vertices[0], new_v1, new_v2, self.vertices[3]], self.direction),
+                            Face([new_v1, self.vertices[1], self.vertices[2], new_v2], self.direction))
                 else:  # second/fourth edges parallel to axis
                     mid = w1*self.vertices[1][i] + w2*self.vertices[2][i]
                     new_v2, new_v3 = self.vertices[2].copy(), self.vertices[3].copy()
                     # print new_v2, new_v3
                     new_v2[i], new_v3[i] = mid, mid
                     print new_v2, new_v3
-                    return (Face([self.vertices[0], self.vertices[1], new_v2, new_v3], self.type, self.direction),
-                            Face([self.vertices[3], new_v3, new_v2, self.vertices[2]], self.type, self.direction))
+                    return (Face([self.vertices[0], self.vertices[1], new_v2, new_v3], self.direction),
+                            Face([self.vertices[3], new_v3, new_v2, self.vertices[2]], self.direction))
 
 
 if __name__ == "__main__":
@@ -81,7 +80,7 @@ if __name__ == "__main__":
     v_2 = np.array([1., 0., 0.])
     v_3 = np.array([1., 1., 0.])
     v_4 = np.array([0., 1., 0.])
-    f = Face([v_1, v_2, v_3, v_4], None, "+z")
+    f = Face([v_1, v_2, v_3, v_4], "+z")
     print f
     for face in f.divide_face("y", (1, 1)):
         print face
