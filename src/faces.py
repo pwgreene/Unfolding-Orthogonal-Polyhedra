e19a3258e2fffdaa8ff59391dc4d609e44ce1cd5
msangha <manjot@mit.edu>
2017-05-17 00:45:38 -0400

    created unfolding tree

--- src/faces.py
+++ src/faces.py
@@ -2,13 +2,12 @@ import numpy as np
 
 class Face(object):
 
-    def __init__(self, vertices, direction, sort_vertices=True):
+    def __init__(self, vertices, direction):
         """
         initialize the face, vertices v1, v2, v3, v4 (np arrays) should be defined in ccw order
         edges of the face are (v1, v2), (v2, v3), (v3, v4), and (v4, v1)
         vertices: list of vertex coordinates
         type: defines the type of face
-        sort_vertices: (bool) sort list of vertices by self.sorted_vertices or not. default=True
         direction: +- x, +- y, or +- z (string)
         """
         self.vertices = vertices
@@ -17,9 +16,6 @@ class Face(object):
             self.direction = "+"+direction
         else:
             self.direction = direction
-        self.center = self.compute_center()
-        if sort_vertices:
-            self.vertices = self.sorted_vertices()
 
     def get_direction(self):
         return self.direction
@@ -108,72 +104,13 @@ class Face(object):
                             Face([self.vertices[3], new_v3, new_v2, self.vertices[2]], self.direction))
 
 
-    def compute_center(self):
-        """
-        :return: average of all the vertices
-        """
-        avg = np.zeros((3,))
-        for v in self.vertices:
-            avg += .25*v
-        return avg
-
-    def sorted_vertices(self):
-        """
-        Return new list of vertices such that first vertex is the closet (resp. ccw) vertex
-        and smallest y-value (still listed in ccw order).
-        :return: new list of vertices. Doesn't modify self.vertices!
-        """
-        if self.direction == "+z":
-            first = sorted(self.vertices, key=lambda z: (z[0], z[1]))[0]
-        elif self.direction == "-z":
-            first = sorted(self.vertices, key=lambda z: (-z[0], z[1]))[0]
-        elif self.direction == "+x":
-            first = sorted(self.vertices, key=lambda x: (-x[2], x[1]))[0]
-        elif self.direction == "-x":
-            first = sorted(self.vertices, key=lambda x: (x[2], x[1]))[0]
-        elif self.direction == "+y":
-            first = sorted(self.vertices, key=lambda y: (-y[0], y[2]))[0]
-        elif self.direction == "-y":
-            first = sorted(self.vertices, key=lambda y: (y[0], y[2]))[0]
-        else:
-            raise TypeError("no direction inputted")
-
-        for i in range(len(self.vertices)):
-            if np.array_equal(first, self.vertices[i]):
-                return self.vertices[i:] + self.vertices[:i]
-
-
-    def is_on_face(self, vertex):
-        """
-        check if the vertex is on the face
-        :param vertex: np array, tuple, or list of coordinates
-        :return: (bool) True if vertex on face, False o.w.
-        """
-        # TODO: implement this
-        return False
-
-
-    def __eq__(self, other):
-        """Override the default Equals behavior"""
-        if not isinstance(other, self.__class__):
-            return False
-
-        this_vertices = {tuple(self.vertices[i]) for i in range(len(self.vertices))}
-        other_vertices = {tuple(other.vertices[i]) for i in range(len(other.vertices))}
-
-        if len(this_vertices.union(other_vertices)) == len(this_vertices) and self.direction == other.direction:
-            return True
-        else:
-            return False
-
 if __name__ == "__main__":
     # TODO: test this with rectangular faces
     v_1 = np.array([0., 0., 0.])
     v_2 = np.array([1., 0., 0.])
     v_3 = np.array([1., 1., 0.])
     v_4 = np.array([0., 1., 0.])
-    f1 = Face([v_1, v_2, v_3, v_4], "+z")
-    f2 = Face([v_4, v_3, v_2, v_1], "-z")
-    f3 = Face([v_4, v_3, v_2, v_1], "+z")
-    assert f1 != f2
-    assert f1 == f3
+    f = Face([v_1, v_2, v_3, v_4], "+z")
+    print f
+    for face in f.divide_face("y", (1, 1)):
+        print face
